77083b641ff93c5f0a37c20eee1ad0776cff4eb6
sachinkizhakkayil <saching1998@cet.ac.in>
2019-11-02 00:06:47 +0530

    api test

--- assets/js/min.js
+++ assets/js/min.js
@@ -7,3 +7,26 @@ function openRequestedPopup(strUrl, strWindowName) {
     windowObjectReference.focus();
   };
 }
+
+
+//API_ROOT = 'http://localhost:3000/'
+API_ROOT = 'https://evening-river-46814.herokuapp.com/'
+// const search_box = ;
+
+
+const getQuote = () => {
+  axios.get(API_ROOT + 'quote',{
+    headers: {
+        'Content-Type': 'application/json'
+      }
+  })
+    .then(function (response) {
+      console.log(response);
+      document.getElementById('quote').innerHTML = response.data.quote.fulltext;
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+}
+
+getQuote()
